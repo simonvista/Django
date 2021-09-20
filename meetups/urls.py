4599dfda245a095ee0540bf70aef74aa919684ea
YL <simon-ling@hotmail.com>
2021-09-20 11:27:59 -0400

    done

--- meetups/urls.py
+++ meetups/urls.py
@@ -2,6 +2,6 @@ from django.urls.conf import path
 from . import views
 
 urlpatterns = [
-    path('meetups/',views.index), #domain.com/meetups
-    path('meetups/<slug:meetup_slug>',views.meetup_details), #domain.com/1st-meetup
+    path('meetups/',views.index,name='all-meetups'), #domain.com/meetups
+    path('meetups/<slug:meetup_slug>',views.meetup_details,name='meetup-detail'), #domain.com/1st-meetup
 ]
