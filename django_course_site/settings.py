975130d8fc31c3c88fe0cdfe00bbd610055e882d
YL <simon-ling@hotmail.com>
2021-09-20 19:34:30 -0400

    done

--- django_course_site/settings.py
+++ django_course_site/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# uploads/images folder will be created under project root folder
+MEDIA_ROOT=BASE_DIR / 'uploads'
+MEDIA_URL='/files/'
