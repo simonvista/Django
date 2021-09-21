b780fff1fc674886b8b0a24a2fda34dacd7f39f7
YL <simon-ling@hotmail.com>
2021-09-20 20:39:39 -0400

    done

--- meetups/models.py
+++ meetups/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 # Create your models here ->Meetups table
+class Location(models.Model):
+    name=models.CharField(max_length=200)
+    address=models.CharField(max_length=300)
+
+    def __str__(self) -> str:
+        return f'{self.name} - {self.address}'
+
 class Meetup(models.Model):
     title=models.CharField(max_length=200)
     slug=models.SlugField(unique=True)
     description=models.TextField()
     image=models.ImageField(upload_to="images")
+    location=models.ForeignKey(Location,on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.title} - {self.slug}'
+
+# one-to-many -> one location has many meetups. one meetup has only one location
